f0d5a5e1c75cb066a65f4189c66382f30762249a
adtak <adtak62@gmail.com>
2022-12-06 22:18:18 +0900

    fix: fix page.py

--- melon-crawler/melon_crawler/page.py
+++ melon-crawler/melon_crawler/page.py
@@ -10,8 +10,8 @@ def parse_search_list(
 ) -> List[Dict[str, str]]:
     list_page = BeautifulSoup(requests.get(url).content, "html.parser")
     for unit in list_page.find_all("h2", class_="property_unit-title"):
-        basic_info_list.append(
-            parse_basic_info(os.environ["ORIGIN"] + unit.find("a").get("href"))
+        basic_info_list += parse_detail(
+            os.environ["ORIGIN"] + unit.find("a").get("href")
         )
     for pagination in list_page.find("div", class_="pagination_set").find_all("a"):
         if "次へ" in pagination.text:
@@ -21,15 +21,49 @@ def parse_search_list(
     return basic_info_list
 
 
-def parse_basic_info(url: str) -> Dict[str, str]:
-    unit_page = BeautifulSoup(requests.get(url).content, "html.parser")
-    main_contents = unit_page.find("div", id="mainContents")
-    for section in main_contents.find_all("div", class_="section_h2"):
-        if "物件概要" in section.find("button").text:
+def parse_detail(url: str) -> Dict[str, str]:
+    detail_page = BeautifulSoup(requests.get(url).content, "html.parser")
+    main_contents = detail_page.find("div", id="mainContents")
+    summary = parse_summary(url, main_contents)
+    layouts = parse_layouts(main_contents)
+    return [layout | summary for layout in layouts]
+
+
+def parse_summary(url, main_contents):
+    summary = None
+    for div_tag in main_contents.find_all("div", class_="section_h2"):
+        if "物件概要" in div_tag.find("button").text:
+            summary = div_tag
             break
-    basic_info = {"URL": url}
-    for th, td in zip(section.find_all("th"), section.find_all("td")):
+    result = {"URL": url}
+    for th, td in zip(summary.find_all("th"), summary.find_all("td")):
         key = th.get_text(strip=True).replace("ヒント", "")
         value = td.get_text(strip=True)
-        basic_info[key] = value
-    return basic_info
+        result[key] = value
+    return result
+
+
+def parse_layouts(main_contents):
+    layout = None
+    for div_tag in main_contents.find_all("div", class_="mt20"):
+        h3_tag = div_tag.find("h3")
+        if h3_tag and "間取り図" in h3_tag:
+            layout = div_tag
+            break
+    if not layout:
+        return [{}]
+    results = []
+    for li in layout.find_all("li"):
+        result = {}
+        result["name"] = li.find("div", class_="icLoupeSide").get_text(strip=True)
+        for dt, dd in zip(li.find_all("dt"), li.find_all("dd")):
+            key = dt.get_text(strip=True).replace("\r\n", "").replace("\t", "")
+            value = (
+                dd.get_text(strip=True)
+                .replace("\r\n", "")
+                .replace("\t", "")
+                .replace("：", "")
+            )
+            result[f"{key}_"] = value
+        results.append(result)
+    return results
