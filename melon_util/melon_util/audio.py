639e4f4e26fc0bf6e47e52731de08c65f42c5f6d
adtak <adtak62@gmail.com>
2023-09-29 16:11:04 +0900

    add feature to split audio by info

--- melon_util/melon_util/audio.py
+++ melon_util/melon_util/audio.py
@@ -1,3 +1,4 @@
+import json
 import re
 from pathlib import Path
 
@@ -7,7 +8,7 @@ from pydub import AudioSegment
 
 
 def main() -> None:
-    pass
+    split(duration=None)
 
 
 def _load_wav(file_path: Path) -> list[AudioSegment]:
@@ -38,20 +39,52 @@ def merge(input_dir: str) -> None:
     combined.export(input_path / "audio.wav", format="wav")
 
 
-def split(input_dir: str, duration: int = 30_000) -> None:
+def split(input_dir: str = "outputs", duration: int = 30_000) -> None:
     input_path = Path(input_dir)
     output_path = input_path / "split"
     output_path.mkdir(exist_ok=True)
     for file in input_path.glob("*.mp3"):
         logger.info(f"Split audio {file.name}")
-        audio = AudioSegment.from_file(input_path / file).set_frame_rate(44100)
-        for i in range(0, len(audio), duration):
-            chunk = audio[i : i + duration]
-            chunk.export(
-                output_path
-                / (re.sub(r"\W+", "_", file.stem) + f" - chunk{i//1000}.wav"),
-                format="wav",
-            )
+        audio = AudioSegment.from_file(file).set_frame_rate(44100)
+        if duration:
+            _split_by_duration(audio, input_path, output_path, duration)
+        else:
+            _split_by_info(audio, file.stem, input_path, output_path)
+
+
+def _split_by_duration(
+    audio: AudioSegment,
+    file: Path,
+    output_path: Path,
+    duration: int = 30_000,
+) -> None:
+    for i in range(0, len(audio), duration):
+        chunk = audio[i : i + duration]
+        chunk.export(
+            output_path / (re.sub(r"\W+", "_", file.stem) + f" - chunk{i//1000}.wav"),
+            format="wav",
+        )
+
+
+def _split_by_info(
+    audio: AudioSegment,
+    audio_id: str,
+    input_path: Path,
+    output_path: Path,
+) -> None:
+    with (input_path / f"{audio_id}.json").open() as f:
+        info = json.load(f)
+    for i, beat_info in enumerate(info):
+        start = beat_info["timestamp"]
+        try:
+            end = info[i + 1]["timestamp"]
+        except IndexError:
+            end = len(audio)
+        chunk = audio[start:end]
+        chunk.export(
+            output_path / (re.sub(r"\W+", "_", audio_id) + f" - chunk{i+1}.wav"),
+            format="wav",
+        )
 
 
 if __name__ == "__main__":
