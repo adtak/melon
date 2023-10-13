2bcf9fd5ca2f9897925e270e80b921913ab03072
adtak <adtak62@gmail.com>
2023-10-13 14:56:39 +0900

    add moviepy

--- melon_util/melon_util/image.py
+++ melon_util/melon_util/image.py
@@ -9,10 +9,10 @@ def main() -> None:
     img_path = Path(os.environ["PROJECT_PATH"])
     img_name = "image.png"
     img = Image.open(img_path / img_name)
-    font = ImageFont.truetype("/System/Library/Fonts/Avenir Next.ttc", 200)
+    font = ImageFont.truetype("/System/Library/Fonts/Avenir Next.ttc", 150)
     ImageDraw.Draw(img).text(
-        (960, 540),
-        "Mix",
+        (640, 360),
+        "AI Lofi Mix",
         "white",
         font=font,
         anchor="mm",
