c2ff58987832e6993b6e95a91ef3d80b773b730e
adtak <adtak62@gmail.com>
2023-10-29 00:02:15 +0900

    add merge movie function

--- melon_util/melon_util/movie.py
+++ melon_util/melon_util/movie.py
@@ -10,27 +10,38 @@ from moviepy.editor import (
 
 def main() -> None:
     project_path = Path("/")
-    fade_movie(project_path)
+    merge_movie(project_path)
+
+
+def merge_movie(project_path: Path) -> None:
+    clip_1 = VideoFileClip(
+        str(project_path / "fade_video_1.mp4"),
+    )
+    clip_2 = VideoFileClip(
+        str(project_path / "fade_video_2.mp4"),
+    )
+    clip_1 = clip_1.set_start(0).set_position((0, 0))
+    clip_2 = clip_2.set_start(9).set_position((0, 0))
+    merged_clip = CompositeVideoClip([clip_1, clip_2])
+    merged_clip.write_videofile(str(project_path / "fade_video.mp4"), fps=60)
 
 
 def fade_movie(project_path: Path) -> None:
     before_clip = VideoFileClip(
-        str(project_path / "image_animation.mp4"),
-    ).loop(duration=15)
+        str(project_path / "image-2-_animation.mp4"),
+    ).loop(duration=18)
     change_clip = VideoFileClip(
-        str(project_path / "image_c_animation.mp4"),
-    ).loop(duration=15)
-
+        str(project_path / "image-3-_animation.mp4"),
+    ).loop(duration=18)
     before_clip = before_clip.set_start(0).set_position((0, 0))
-    change_clip = change_clip.crossfadein(10).set_start(0).set_position((0, 0))
+    change_clip = change_clip.crossfadein(12).set_start(0).set_position((0, 0))
     merged_clip = CompositeVideoClip([before_clip, change_clip])
-    merged_clip.write_videofile(str(project_path / "fade_video_1.mp4"), fps=60)
+    merged_clip.write_videofile(str(project_path / "fade_video_2.mp4"), fps=60)
 
 
 def fade_image(project_path: Path) -> None:
     before_clip = ImageClip(str(project_path / "image.png"), duration=30)
     change_clip = ImageClip(str(project_path / "image_c.png"), duration=30)
-
     before_clip = before_clip.crossfadeout(30).set_start(0).set_position((0, 0))
     change_clip = change_clip.crossfadein(30).set_start(0).set_position((0, 0))
     merged_clip = CompositeVideoClip([before_clip, change_clip])
@@ -45,14 +56,12 @@ def challenge(project_path: Path) -> None:
         [before_clip, change_clip, after_clip],
         method="compose",
     )
-
     bar_clip = ImageClip(str(project_path / "bar.png"), duration=12)
     bar_clip = bar_clip.set_position(("center", 800))
     target_clip = ImageClip(str(project_path / "target.png"), duration=12)
     target_clip = target_clip.set_position((400, 800))
     seek_clip = ImageClip(str(project_path / "seek.png"), duration=12)
     seek_clip = seek_clip.set_position(lambda t: (t * 50, 800))
-
     composite = CompositeVideoClip(
         [base_clip, bar_clip, target_clip, seek_clip],
         size=base_clip.size,
