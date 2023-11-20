f97bbcae99317f65d0550090089eafc0c13a7b55
adtak <adtak62@gmail.com>
2023-11-20 09:35:18 +0900

    update to use settings image.py

--- melon_util/melon_util/image.py
+++ melon_util/melon_util/image.py
@@ -1,23 +1,35 @@
+import json
 import os
 from pathlib import Path
+from typing import TypedDict
 
 from dotenv import load_dotenv
 from PIL import Image, ImageDraw, ImageFont
 
 
+class Settings(TypedDict):
+    text: str
+
+
+def load_settings() -> Settings:
+    with Path("image.settings.json").open("r") as f:
+        return json.load(f)
+
+
 def main() -> None:
-    img_path = Path(os.environ["PROJECT_PATH"])
+    prj_path = Path(os.environ["PROJECT_PATH"])
     img_name = "image.png"
-    img = Image.open(img_path / img_name)
-    font = ImageFont.truetype("/System/Library/Fonts/Avenir Next.ttc", 150)
+    settings = load_settings()
+    img = Image.open(prj_path / img_name)
+    font = ImageFont.truetype("/System/Library/Fonts/Avenir Next.ttc", 130)
     ImageDraw.Draw(img).text(
-        (640, 360),
-        "AI Lofi Mix",
+        (640, 570),
+        settings["text"],
         "white",
         font=font,
         anchor="mm",
     )
-    img.save(img_path / ("font_" + img_name))
+    img.save(prj_path / ("font_" + img_name))
 
 
 if __name__ == "__main__":
