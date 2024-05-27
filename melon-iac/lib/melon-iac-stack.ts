268681a8f6b3ecf452549d9e67e8e63029a8fe35
adtak <adtak62@gmail.com>
2024-05-27 18:53:41 +0900

    add lambda function

--- melon-iac/lib/melon-iac-stack.ts
+++ melon-iac/lib/melon-iac-stack.ts
@@ -1,16 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sns from "aws-cdk-lib/aws-sns";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { SnsDestination } from "aws-cdk-lib/aws-lambda-destinations";
 
 export class MelonIacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const errorTopic = new sns.Topic(this, "errorTopic", {
+      topicName: "errorTopic",
+      displayName: "errorTopic",
+    });
+    errorTopic.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["sns:Publish"],
+        principals: [new iam.ServicePrincipal("events.amazonaws.com")],
+        resources: [errorTopic.topicArn],
+      })
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MelonIacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new NodejsFunction(this, "crawlerFunction", {
+      functionName: "melonCrawlerFunction",
+      entry: "lambda/crawler/index.ts",
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      timeout: cdk.Duration.seconds(60 * 10),
+      environment: {},
+      onFailure: new SnsDestination(errorTopic),
+    });
   }
 }
