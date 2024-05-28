8fbee04e6860eb5012e6c9c4d955df01cad92192
adtak <adtak62@gmail.com>
2024-05-29 01:55:24 +0900

    update scraping

--- melon-iac/lambda/crawler/index.ts
+++ melon-iac/lambda/crawler/index.ts
@@ -4,13 +4,15 @@ import axios, { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
 
 export const handler: Handler = async (event, context) => {
-  const url = "https://www.google.com";
+  const url = "";
   console.log(`url: ${url}`);
   try {
     const res: AxiosResponse = await axios.get(url);
     const $ = cheerio.load(res.data);
-    const title = $("title").text();
-    console.log(`title: ${title}`);
+    const span = $("span").filter((_, element) => {
+      return $(element).text().includes("カートに追加する");
+    });
+    console.log(`span: ${span.text()}`);
   } catch (e) {
     if (axios.isAxiosError(e)) {
       console.log(e.message);
