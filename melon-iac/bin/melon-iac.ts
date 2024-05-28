56fbab3eb122ffb24c1ee40ca9bf12f8bde05df9
adtak <adtak62@gmail.com>
2024-05-28 15:39:26 +0900

    update to use aws account from dotenv

--- melon-iac/bin/melon-iac.ts
+++ melon-iac/bin/melon-iac.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import "source-map-support/register";
+import "dotenv/config";
 import * as cdk from "aws-cdk-lib";
 import { MelonIacStack } from "../lib/melon-iac-stack";
 
 const app = new cdk.App();
 const env = {
-  account: "111111111111",
+  account: process.env.AWS_ACCOUNT,
   region: "ap-northeast-1",
 };
 new MelonIacStack(app, "MelonIacStack", { env });
