ddb593ce4f24dde86f8d3cabc4a0d527bf81bf2b
adtak <adtak62@gmail.com>
2024-05-29 01:11:40 +0900

    await call get function

--- melon-iac/lambda/crawler/index.ts
+++ melon-iac/lambda/crawler/index.ts
@@ -1,20 +1,21 @@
 import { Handler } from "aws-lambda";
 
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
 
 export const handler: Handler = async (event, context) => {
   const url = "https://www.google.com";
-  console.log(url);
-  axios
-    .get(url)
-    .then((res: AxiosResponse) => {
-      const { data } = res;
-      const $ = cheerio.load(data);
-      const title = $("title").text();
-      console.log(title);
-    })
-    .catch((e: AxiosError) => {
+  console.log(`url: ${url}`);
+  try {
+    const res: AxiosResponse = await axios.get(url);
+    const $ = cheerio.load(res.data);
+    const title = $("title").text();
+    console.log(`title: ${title}`);
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
       console.log(e.message);
-    });
+    } else {
+      console.log("unknown error");
+    }
+  }
 };
