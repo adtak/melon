789faac592eaac7f8513830267500a7c4a0b4409
adtak <adtak62@gmail.com>
2024-05-31 09:09:14 +0900

    refactoring crawling function

--- melon-iac/lambda/crawler/index.ts
+++ melon-iac/lambda/crawler/index.ts
@@ -14,14 +14,14 @@ export const handler: Handler = async (event, context) => {
 
   const urlT = process.env.SITE_T;
   if (urlT) {
-    const instockUrl = await crawlSpan(urlT);
+    const instockUrl = await crawlingSpan(urlT);
     if (instockUrl) {
       instockUrls.push(instockUrl);
     }
   }
   const urlK = process.env.SITE_K;
   if (urlK) {
-    const instockUrl = await crawlButton(urlK);
+    const instockUrl = await crawlingButton(urlK);
     if (instockUrl) {
       instockUrls.push(instockUrl);
     }
@@ -37,24 +37,33 @@ export const handler: Handler = async (event, context) => {
   }
 };
 
-const crawlSpan = async (url: string) => {
-  console.log(`Crawling span tag. URL: ${url}`);
-  const res: AxiosResponse = await axios.get(url);
-  const $ = cheerio.load(res.data);
-  const span = $("span").filter((_, element) => {
-    return $(element).text().includes("カートに追加する");
-  });
-  if (span.text()) {
-    console.log("in stock");
-    return url;
-  } else {
-    console.log("sold out");
-    return undefined;
-  }
+const crawlingSpan = async (url: string) => {
+  const parseFunc = (res: AxiosResponse) => {
+    const $ = cheerio.load(res.data);
+    const span = $("span").filter((_, element) => {
+      return $(element).text().includes("カートに追加する");
+    });
+    return span.text();
+  };
+  return await crawlingURL(url, parseFunc);
 };
 
-const crawlButton = async (url: string) => {
-  console.log(`Crawling button tag. URL: ${url}`);
+const crawlingButton = async (url: string) => {
+  const parseFunc = (res: AxiosResponse) => {
+    const $ = cheerio.load(res.data);
+    const button = $("button").filter((_, element) => {
+      return $(element).text().includes("カートに入れる");
+    });
+    return button.text();
+  };
+  return await crawlingURL(url, parseFunc);
+};
+
+const crawlingURL = async (
+  url: string,
+  parseFunc: (res: AxiosResponse) => string | undefined
+) => {
+  console.log(`Crawling URL: ${url}`);
   const res: AxiosResponse = await axios.get(url, {
     responseType: "arraybuffer",
     transformResponse: (data) => {
@@ -67,11 +76,8 @@ const crawlButton = async (url: string) => {
       return iconv.decode(data, encoding);
     },
   });
-  const $ = cheerio.load(res.data);
-  const span = $("button").filter((_, element) => {
-    return $(element).text().includes("カートに入れる");
-  });
-  if (span.text()) {
+  const text = parseFunc(res);
+  if (text) {
     console.log("in stock");
     return url;
   } else {
